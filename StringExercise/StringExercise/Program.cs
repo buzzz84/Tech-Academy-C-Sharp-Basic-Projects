c8abf1f7814c6222a28a59984a27940e736d6d17
Ryan Buzard <buzzz84@hotmail.com>
2018-09-19 15:25:04 -0700

    StringExercise Completed
    StringExercise Completed

---
+++ StringExercise/StringExercise/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StringExercise
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string fname = "Rick";
+            string lname = "James";
+            string phone = "867-5309";
+            fname = fname.ToUpper();
+            Console.WriteLine("Hello, my first name is " + fname + " and my last name is " + lname + " and you can reach me at " + phone);
+            Console.ReadLine();
+
+            StringBuilder sb = new StringBuilder();
+            sb.Append("My name is Rick James. ");
+            sb.Append("I like to party with Charlie Murphy. ");
+            sb.Append("He is one of my best friends ever.");
+
+            Console.WriteLine(sb);
+            Console.ReadLine();
+        }
+    }
+}
---
+++ StringPractice/StringPractice/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StringPractice
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //string name = "ryan";
+
+            //string quote = "The man said, \"Hello\", Ryan. \n Hello on a new line. \n \t Hello on a tab";
+            //string filename = @"C:\\Users\\Ryan";
+
+            //bool trueOrFalse = name.Contains("s");
+            //trueOrFalse = name.EndsWith("s");
+
+            //int length = name.Length;
+
+            //name = name.ToUpper();
+
+            //name = name.ToLower();
+
+            //Console.WriteLine(trueOrFalse);
+            //Console.ReadLine();
+
+            StringBuilder sb = new StringBuilder();
+
+            sb.Append("My name is Ryan");
+
+
+            Console.WriteLine(sb);
+            Console.ReadLine();
+        }
+    }
+}
