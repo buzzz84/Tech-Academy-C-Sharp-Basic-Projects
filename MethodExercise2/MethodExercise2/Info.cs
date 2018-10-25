fd13f5907da9c283913dcf52d7887094c7ed11a0
Ryan Buzard <buzzz84@hotmail.com>
2018-10-25 15:13:26 -0700

    MethodExercise2 completed
    MethodExercise2 completed

---
+++ MethodExercise2/MethodExercise2/Info.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodExercise2
+{
+    public class Info
+    {
+        //public static int Numbers (int num1, int num2)
+        //{
+        //    int results = num1 * Convert.ToInt32(num2);
+        //    return results;
+        //}
+        public static int Numbers (int num1, int num2 = 15)
+        {
+            int results = num1 * Convert.ToInt32(num2);
+            return results;
+        }
+    }
+}
+
---
+++ MethodExercise2/MethodExercise2/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodExercise2
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Info Option = new Info();
+
+            Console.WriteLine("Please type in a number.");
+            int num1 = Int32.Parse(Console.ReadLine());
+            int userInput = num1;
+            Console.WriteLine("**OPTIONAL** \n You may pick another number unless you would like number already assigned.");
+            int num2 = 15;
+            string standardInput = Console.ReadLine();
+
+            if (string.IsNullOrEmpty(standardInput))
+            {
+                Console.WriteLine(Info.Numbers(userInput, Convert.ToInt32(num2)));
+            }
+            else
+            {
+                Console.WriteLine(Info.Numbers(userInput, Convert.ToInt32(standardInput)));
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
