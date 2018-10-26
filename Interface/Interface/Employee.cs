8dea949a06acc14c483eac3130f562727e830633
Ryan Buzard <buzzz84@hotmail.com>
2018-10-26 12:10:35 -0700

    Interface Completed
    Interface Completed

---
+++ Interface/Interface/Employee.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Interface
+{
+    class Employee : Person, IQuittable
+    {
+        public override void SayName()
+        {
+            Console.WriteLine($"Name: [{FirstName} {LastName}]");
+        }
+        public void Quit()
+        {
+            Console.WriteLine("I am quitting right now, I just won the MegaMillions!!!");
+            Console.ReadLine();
+        }
+    }
+}
--- TwentyOneClassesAndObjects/TwentyOneClassesAndObjects/Program.cs
+++ TwentyOneClassesAndObjects/TwentyOneClassesAndObjects/Program.cs
@@ -11,20 +11,20 @@ namespace TwentyOneClassesAndObjects
         static void Main(string[] args)
         {
             /*Game game = new TwentyOneGame();*/ //PolyMorphism- ability of a class to morph into its inheriting class and give certain advantages
-            TwentyOneGame game = new TwentyOneGame();
-            game.Players = new List<string>() { "Jesse", "Bill", "Bob" };
-            game.ListPlayers();
-            Console.ReadLine();
-
-            //Deck deck = new Deck();
-            //deck.Shuffle(3);
-
-            //foreach (Card card in deck.Cards)
-            //{
-            //    Console.WriteLine(card.Face + " of " + card.Suit);
-            //}
-            //Console.WriteLine(deck.Cards.Count);
+            //TwentyOneGame game = new TwentyOneGame();
+            //game.Players = new List<string>() { "Jesse", "Bill", "Bob" }; -- 
+            //game.ListPlayers();
             //Console.ReadLine();
+
+            Deck deck = new Deck();
+            deck.Shuffle(3);
+
+            foreach (Card card in deck.Cards)
+            {
+                Console.WriteLine(card.Face + " of " + card.Suit);
+            }
+            Console.WriteLine(deck.Cards.Count);
+            Console.ReadLine();
         }
 
         
