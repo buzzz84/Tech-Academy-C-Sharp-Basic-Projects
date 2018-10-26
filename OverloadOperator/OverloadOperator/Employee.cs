c83e7712b83c66afe9007af2b8540bd2c5c26851
Ryan Buzard <buzzz84@hotmail.com>
2018-10-26 13:36:41 -0700

    updated OverloadOperator
    updated OverloadOperator

--- OverloadOperator/OverloadOperator/Employee.cs
+++ OverloadOperator/OverloadOperator/Employee.cs
@@ -6,7 +6,44 @@ using System.Threading.Tasks;
 
 namespace OverloadOperator
 {
-    class Employee
+    class Employee : Person, IQuittable
     {
+        public int Id { get; set; }
+
+        public override void SayName()
+        {
+            Console.WriteLine($"Name: [{FirstName} {LastName} {Id}]");
+        }
+        public void Quit()
+        {
+            Console.WriteLine("I am quitting right now, I just won the MegaMillions!!!");
+            Console.ReadLine();
+        }
+        public static Boolean operator ==(Employee employee1, Employee employee2)
+        {
+            if (employee1.Id == employee2.Id)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+        public static Boolean operator !=(Employee employee1, Employee employee2)
+        {
+            if (employee1.Id != employee2.Id)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+        public override int GetHashCode()
+        {
+            return Id.GetHashCode();
+        }
     }
 }
--- OverloadOperator/OverloadOperator/Person.cs
+++ OverloadOperator/OverloadOperator/Person.cs
@@ -6,7 +6,10 @@ using System.Threading.Tasks;
 
 namespace OverloadOperator
 {
-    class Person
+    public abstract class Person
     {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public abstract void SayName();
     }
 }
